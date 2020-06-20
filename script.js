f50809d98cc60478291edbdbad67bb05b3f435f7
dustinlo <lo.ch@husky.neu.edu>
2020-06-19 23:42:24 -0700

    Added my little project

---
+++ script.js
@@ -0,0 +1,15 @@
+let css = document.querySelector('h3')
+let c1 = document.querySelector('.color1')
+let c2 = document.querySelector('.color2')
+let body = document.querySelector('#gradient')
+
+let setGradient = ()=> {
+	body.style.background = 'linear-gradient(to right, ' 
+	+ c1.value + ', ' 
+	+ c2.value + ')'
+
+	css.textContent = body.style.background
+}
+c1.addEventListener('input', setGradient)
+c2.addEventListener('input', setGradient)
+
